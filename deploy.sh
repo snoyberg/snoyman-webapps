a2089e5c6603262ac34a0206a326a264d1e4e60a
Michael Snoyman <michael@fpcomplete.com>
2016-08-28 11:39:22 +0000

    Use -f for docker tag

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ set -ex
 cd $(dirname $0)
 stack image container
 NAME=snoyberg/snoyman-webapps:sha-$(git rev-parse HEAD)
-docker tag snoyberg/snoyman-webapps:latest $NAME
+docker tag -f snoyberg/snoyman-webapps:latest $NAME
 docker push $NAME
 kubectl set image deployments/snoyman-webapps webapps=$NAME
