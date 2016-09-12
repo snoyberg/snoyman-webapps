58e3c4469eb3b75bfdfe5227253470b14f48920d
Michael Snoyman <michael@snoyman.com>
2016-09-12 10:12:35 +0300

    Add --docker-auto-pull

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -ex
 
 cd $(dirname $0)
-stack image container
+stack --docker-auto-pull image container
 NAME=snoyberg/snoyman-webapps:sha-$(git rev-parse HEAD)
 docker tag -f snoyberg/snoyman-webapps:latest $NAME
 docker push $NAME
